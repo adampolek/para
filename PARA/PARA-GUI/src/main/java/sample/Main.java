e4ed671c7cbc8303dc1b3d42d23ec5fdb73f9b40
Mateusz Samluk <mazsak97@gmail.com>
2018-11-26 13:08:16 +0100

    modul GUI zmiany

--- PARA/PARA-GUI/src/main/java/sample/Main.java
+++ PARA/PARA-GUI/src/main/java/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/FXML/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/FXML/MenuFirst/Start.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
--- PARA/PARA-GUI/src/main/java/sample/MenuFirst/Controller.java
+++ PARA/PARA-GUI/src/main/java/sample/MenuFirst/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package sample.MenuFirst;
 
 public class Controller {
 }
