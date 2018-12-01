e406d06284e7e2565c8319d1a472eb1900f3ed00
Adam Polejczuk <adampolejczuk@gmail.com>
2018-12-01 16:30:12 +0100

    tak

--- PARA/PARA-Swing/src/main/java/CharactersList/CharactersList.java
+++ PARA/PARA-Swing/src/main/java/CharactersList/CharactersList.java
@@ -1,14 +1,25 @@
 package CharactersList;
 
+import Characters.Character;
+import Gameplay.Game;
+
 import javax.swing.*;
+import javax.swing.event.ListDataEvent;
+import javax.swing.event.ListDataListener;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.*;
+import java.util.ArrayList;
+import Style.Style;
 
 public class CharactersList{
 
     DefaultListModel listModel;
     JList list;
+    final JPanel HeroDetails = new JPanel();;
     private int screenWidth;
     private int screenHeight;
+    private ArrayList<Character> characters = new ArrayList<>();
 
 
     public CharactersList(){
@@ -18,11 +29,27 @@ public class CharactersList{
         screenHeight = (int) screenSize.getHeight();
     }
 
-    public void addHeroesToList(){
-        listModel.addElement(new Item("name1", new ImageIcon("warrior.png")));
-        listModel.addElement(new Item("name2", new ImageIcon("dwarf.png")));
-        listModel.addElement(new Item("name3", new ImageIcon("wizard.png")));
-        listModel.addElement(new Item("name4", new ImageIcon("archer.png")));
+    public CharactersList(Game game, final JFrame panel){
+        listModel = new DefaultListModel();
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        screenWidth = (int) screenSize.getWidth();
+        screenHeight = (int) screenSize.getHeight();
+        addHeroesToList(game);
+        showList(panel);
+        list.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                System.out.println("GOOOL, BRAWO POLAAACYYY!!!!!!");
+                createHeroDetail(panel);
+            }
+        });
+    }
+
+    public void addHeroesToList(Game game){
+        characters = (ArrayList<Character>) game.getUser().getUsersCharacters();
+        for (Character ch: characters) {
+            listModel.addElement(new Item(ch.getName(), new ImageIcon(getImageName(ch.getType()))));
+        }
         list = new JList(listModel);
         list.setCellRenderer(new Renderer());
     }
@@ -34,10 +61,55 @@ public class CharactersList{
         list.setVisible(true);
     }
 
-    public void showListInFrame(JFrame frame){
-        frame.add(list);
+    public void showList(JFrame panel){
+        panel.add(list);
         list.setBounds((int)(screenWidth/(double)1.2),0,screenWidth-(int)(screenWidth/(double)1.2),screenHeight);
         list.setModel(listModel);
         list.setVisible(true);
+    }
+
+//    public void showListInFrame(JFrame frame){
+//        frame.add(list);
+//        list.setBounds((int)(screenWidth/(double)1.2),0,screenWidth-(int)(screenWidth/(double)1.2),screenHeight);
+//        list.setModel(listModel);
+//        list.setVisible(true);
+//    }
+
+    public String getImageName(String klasa){
+        if (klasa.equals("Dwarf")) {
+            return "dwarf.png";
+        } else if (klasa.equals("Archer")) {
+            return "archer.png";
+        } else if (klasa.equals("Warrior")) {
+            return "warrior.png";
+        } else if (klasa.equals("Wizard")) {
+            return "wizard.png";
+        }else if(klasa.equals("Character")){
+            return "character.png";
+        } else {
+            return "oh, crap!";
+        }
+    }
+
+    public void createHeroDetail(JFrame frame){
+        frame.add(HeroDetails);
+        HeroDetails.setBounds(screenWidth/3, screenHeight/3, screenWidth/3, screenHeight/3);
+        HeroDetails.setVisible(true);
+        HeroDetails.setBackground(Color.black);
+        JButton backButton = new JButton("BACK");
+        int panelWidth = HeroDetails.getWidth();
+        int panelHeight = HeroDetails.getHeight();
+        Style.styleButtonSimple(backButton, panelWidth / 4, panelHeight / 18, screenHeight/60);
+        backButton.setBounds((int) (panelWidth / (1.4)), (int) (panelHeight / 1.1), panelWidth / 4, panelHeight / 18);
+        HeroDetails.add(backButton);
+        int index;
+        index = list.getSelectedIndex();
+        JLabel imageLabel = new JLabel(new ImageIcon(getImageName(characters.get(index).getType())));
+        imageLabel.setBounds(panelWidth/40,panelHeight/30, panelHeight/4, panelHeight/4);
+        HeroDetails.add(imageLabel);
+        JLabel charactersName = new JLabel(characters.get(index).getName());
+        charactersName.setBounds(panelWidth/4, panelHeight/30, (int)(panelHeight/(1.2)), panelHeight/4);
+        Style.styleTitle(charactersName, screenHeight/40);
+        HeroDetails.add(charactersName);
     }
 }
